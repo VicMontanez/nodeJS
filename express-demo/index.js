b2b686ead7a5807fd201448952f491a619fbcf16
VicMontanez <v_montanez@hotmail.com>
2020-01-07 22:47:33 -0500

    updated error handling

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,3 +1,4 @@
+const Joi = require('joi');
 const express = require("express");
 const app = express();
 
@@ -19,9 +20,15 @@ app.get("/api/courses", (req, res) => {
 
 app.post("/api/courses", (req, res) 
 => {
-  if (!req.body.name || req.body.name.length < 3) {
+  const schema = {
+    name: Joi.string().min(3).required()
+  };
+
+  const result = Joi.validate(req.body, schema);
+
+  if (result.error) {
     //400 Bad Request
-    res.status(400).send('Name is required and should be minimum 3 characters')
+    res.status(400).send(result.error.details[0].message);
     return;
   }
   const course = {
