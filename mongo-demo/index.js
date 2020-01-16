d807a58376ccb44bbd32ce9881135a8b9b19a29c
VicMontanez <v_montanez@hotmail.com>
2020-01-15 21:57:04 -0500

    added logical operators

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -32,9 +32,11 @@ async function getCourses() {
     // .find({
     //     author: 'Mosh', isPublished: true
     // })
+    .find()
+    .or([{ author: "Mosh" }, { isPublished: true }]) //logical operators
     // .find({ price: { $gte: 10, $lte: 20} }) //greater than or equal to 10; less than or equal to 20
-    .find({ price: { $in: [10, 15, 20] } }) //multiple values
-    .limit(10)
+    // .find({ price: { $in: [10, 15, 20] } }) //multiple values
+    // .limit(10)
     .sort({ name: 1 })
     .select({ name: 1, tags: 1 });
   console.log(courses);
