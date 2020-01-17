7b783aeca930ce84d09ebabf8b0dfd6668537c58
VicMontanez <v_montanez@hotmail.com>
2020-01-17 18:30:42 -0500

    added files

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -6,7 +6,7 @@ mongoose
   .catch(err => console.error("Could not connect to MongoDB...", err));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
