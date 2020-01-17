2278064f2961266222f8492f263d6097d894b9e9
VicMontanez <v_montanez@hotmail.com>
2020-01-17 18:28:48 -0500

    added files

--- async-demo/index.js
+++ async-demo/index.js
@@ -1,15 +1,29 @@
+//Promise-based approach
 console.log("Before");
-getUser(1)
-  .then(user => getRepositories(user.gitHubUsername))
-  .then(repos => getCommits(repos[0]))
-  .then(commits => console.log("Commits", commits))
-  .catch(err => console.log("Error", err.message));
+// getUser(1)
+//   .then(user => getRepositories(user.gitHubUsername))
+//   .then(repos => getCommits(repos[0]))
+//   .then(commits => console.log("Commits", commits))
+//   .catch(err => console.log("Error", err.message));
+
+//Async and Await approach
+async function displayCommits() {
+  try {
+    const user = await getUser(1);
+    const repos = await getRepositories(user.gitHubUsername);
+    const commits = await getCommits(repos[0]);
+    console.log(commits);
+  } catch (err) {
+    console.log("Error", err.message);
+  }
+}
+displayCommits();
 
 console.log("After");
 
-function getRepositories(user) {
-  getRepositories(user.gitHubUsername, getCommits);
-}
+// function getRepositories(user) {
+//   getRepositories(user.gitHubUsername, getCommits);
+// }
 
 function getCommits(repo) {
   return new Promise((resolve, reject) => {
@@ -37,7 +51,8 @@ function getRepositories(username) {
   return new Promise((resolve, reject) => {
     setTimeout(() => {
       console.log("Getting repo...");
-      resolve(["repo1", "repo2", "repo3"]);
+      //   resolve(["repo1", "repo2", "repo3"]);
+      reject(new Error("Could not get the repos"));
     }, 2000);
   });
 }
