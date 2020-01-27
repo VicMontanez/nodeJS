11d4c000e159aaac0199c4fb716d6f20558d7e83
VicMontanez <v_montanez@hotmail.com>
2020-01-26 22:16:01 -0500

    added files

--- mongo-demo/index.js
+++ mongo-demo/index.js
@@ -73,12 +73,12 @@ async function getCourses() {
   const pageNumber = 2;
   const pageSize = 10;
 
-  const courses = await Course.find({ author: "Mosh", isPublished: true })
-    .skip((pageNumber - 1) * pageSize)
-    .limit(pageSize)
+  const courses = await Course.find({ _id: "12345" })
+    // .skip((pageNumber - 1) * pageSize)
+    // .limit(pageSize)
     .sort({ name: 1 })
-    .select({ name: 1, tags: 1 });
-  console.log(courses);
+    .select({ name: 1, tags: 1, price });
+  console.log(courses.price);
 }
 
 async function updateCourse(id) {
